85f6f2b765ce05b5b406304df9288956ce87751e
Avinash <95042051+Avinash7223@users.noreply.github.com>
2024-03-06 04:50:49 +0000

    .

---
+++ task_5/Java_program/DateServlet.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.Date; // Import Date class
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+
+public class DateServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Get the HttpSession object.
+        HttpSession hs = request.getSession(true);
+        
+        // Get writer.
+        response.setContentType("text/html");
+        PrintWriter pw = response.getWriter();
+        pw.print("<B>");
+        
+        // Display date/time of last access.
+        Date date = (Date) hs.getAttribute("date");
+        if (date != null) {
+            pw.print("Last access: " + date + "<br>");
+        }
+        
+        // Display current date/time.
+        date = new Date();
+        hs.setAttribute("date", date);
+        pw.println("Current date: " + date);
+    }
+}
